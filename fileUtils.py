a4e78a8f8fd9e84f9590eb859ff18ea1d7e5b09c
Allen <15869157637@163.com>
2018-06-21 12:36:52 +0800

    quant apple

---
+++ fileUtils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# @Time    : 2018/6/5 15:05
+# @Author  : Allen
+# @File    : fileUtils.py
+
+import os
+
+parent_dir = os.path.dirname(__file__)
+data_dir = os.path.abspath(os.path.join(parent_dir, "data_file"))
+temp_dir = os.path.abspath(os.path.join(parent_dir, "temp"))
+csv_dir = os.path.abspath(os.path.join(data_dir, "csv"))
+st_dir = os.path.abspath(os.path.join(csv_dir, "ST"))
+batch_data_dir = os.path.abspath(os.path.join(data_dir, "batch"))
